b500fcae2f22e9dfc9dbdeac1ef0794195c2299b
SNIAPA <wasilewskihuberts@gmail.com>
2022-10-29 15:09:24 +0200

    working on the wildchopper and some utility functions

--- src/Bot.ts
+++ src/Bot.ts
@@ -4,9 +4,10 @@ import minecraftData from 'minecraft-data'
 import mineflayer from 'mineflayer'
 const radar = require('mineflayer-radar')(mineflayer)
 import cmd from 'mineflayer-cmd'
-import { pathfinder, Movements, Pathfinder } from 'mineflayer-pathfinder'
+import { ComputedPath ,pathfinder, Movements, goals as Goals, Pathfinder } from 'mineflayer-pathfinder'
 import Role from './Role'
 import WildChopper from './roles/WildChopper'
+import { Entity } from 'prismarine-entity'
 
 type RoleName = "WILD_CHOPPER"
 
@@ -30,6 +31,9 @@ export default class Bot{
         this.role.execute()
       }
     })    
+    this.mBot.on("login",()=>{
+      console.log(`${this.mBot.username} is up`)
+    })
   }
 
   setRole = (role:RoleName)=>{
@@ -42,5 +46,24 @@ export default class Bot{
     this.role = selectedRole
   }
 
+  getPathTo = async (goal: Goals.Goal, timeout = 5000) => {
+    const path = this.mBot.pathfinder.getPathFromTo( 
+      this.mBot.pathfinder.movements,
+      this.mBot.entity.position,
+      goal,
+      {timeout}
+    )
+    let result
+    do
+      result = path.next()
+    while(result.value.result.status == 'partial')
+    return result.value.result as ComputedPath
+  } 
+
+  pickup = async (entity: Entity) => {
+    const goal = new Goals.GoalNear(entity.position.x,entity.position.y,entity.position.z,0.4)
+    const path = await this.getPathTo(goal)
+    await this.mBot.pathfinder.goto(goal)
+  }
 }
 
--- src/roles/WildChopper.ts
+++ src/roles/WildChopper.ts
@@ -1,123 +1,97 @@
 import { Vec3 } from 'vec3'
-import { pathfinder, Movements, goals } from 'mineflayer-pathfinder'
+import {
+  ComputedPath,
+  pathfinder,
+  Movements,
+  goals as Goals,
+} from 'mineflayer-pathfinder'
 import Bot from '../Bot'
 import Role from '../Role'
-
-const GoalNear = goals.GoalNear
+import { Block } from 'prismarine-block'
 
 export default class WildChopper extends Role {
+  blacklist: Vec3[] = []
+
   constructor(bot: Bot) {
     super(bot)
-
   }
-  async execute(): Promise<void>{
-    console.log('loaded')
-    const blacklist: Vec3[] = []
-    while (true) {
-      const block = this.bot.mBot.findBlock({
-        matching: (x) =>
-          x.name.endsWith('log') &&
-          !blacklist.find((y) => y.distanceTo(x.position) == 0),
-        maxDistance: 100,
-      })
-      if (!block) break
 
-      const pos = block.position
-
-      if (this.bot.mBot.canDigBlock(block)) {
-        try {
-          await this.bot.mBot.equip(
-            this.bot.mBot.pathfinder.bestHarvestTool(block)!,
-            'hand'
-          )
-          console.log(`digging ${pos}`), await this.bot.mBot.dig(block, true)
-        } catch (e) {
-          console.log(`failed to dig ${pos}`)
-          blacklist.push(pos)
-        }
-        continue
-      }
-
-      // get path
-      const goal = new GoalNear(pos.x, pos.y, pos.z, 3)
-      const path = this.bot.mBot.pathfinder.getPathTo(
-        this.bot.mBot.pathfinder.movements,
-        goal
-      )
-      if (path.status != 'success') {
-        console.log(`cant generatoe path to ${pos}`)
-        blacklist.push(pos)
-        continue
-      }
-
-      try {
-        this.bot.mBot.pathfinder.tickTimeout = 50
-        this.bot.mBot.pathfinder.thinkTimeout = 50
-        await this.bot.mBot.pathfinder.goto(goal)
-      } catch (e) {
-        console.log(`failed to reach ${pos}`)
-        blacklist.push(pos)
-      }
-
-      // can dig
-      if (!block || !this.bot.mBot.canDigBlock(block)) {
-        console.log(`cant dig${pos}`)
-        blacklist.push(pos)
-        continue
-      }
-      // dig
+  chop = async (block: Block) => {
+    let repeat
+    do {
+      repeat = false
       try {
         await this.bot.mBot.equip(
           this.bot.mBot.pathfinder.bestHarvestTool(block)!,
           'hand'
         )
-        console.log(`digging ${pos}`), await this.bot.mBot.dig(block, true)
+        await this.bot.mBot.dig(block, true)
       } catch (e) {
-        console.log(`failed to dig ${pos}`)
-        blacklist.push(pos)
+        repeat = true
       }
+    } while (repeat)
+  }
 
-      // pickup
-      // currently it pickups all items in the radius bevause there is no way to get the entity name
-      for (const entity in this.bot.mBot.entities) {
-        const val = this.bot.mBot.entities[entity]
-        if (
-          val.position.distanceTo(this.bot.mBot.entity.position) > 10 ||
-          val.name != 'Item' ||
-          this.bot.mcData.blocks[(val.metadata[10] as any).blockId] == undefined ||
-          !this.bot.mcData.blocks[(val.metadata[10] as any).blockId].name.endsWith(
-            'log'
-          )
-        )
-          continue
+  findNextLog = async () => {
+    return this.bot.mBot.findBlock({
+      matching: (x) =>
+        x.name.endsWith('log') &&
+        !this.blacklist.find((y) => y.distanceTo(x.position) == 0),
+      maxDistance: 1000,
+    })
+  }
 
-        const goal = new GoalNear(
-          val.position.x,
-          val.position.y,
-          val.position.z,
-          0.1
-        )
-        const path = this.bot.mBot.pathfinder.getPathTo(
-          this.bot.mBot.pathfinder.movements,
-          goal
-        )
-        if (path.status != 'success') {
-          console.log(`cant generatoe path to ${pos}`)
-          blacklist.push(pos)
-          continue
-        }
-        try {
-          console.log(
-            `picking up ${
-              this.bot.mcData.blocks[(val.metadata[10] as any).blockId].name
-            } ${val.position}`
-          )
-          await this.bot.mBot.pathfinder.goto(goal)
-        } catch (e) {
-          console.log(`failed to pickup ${val.position}`)
-        }
-      }
+  pickupLogsAround = async () => {
+    for (const entity in this.bot.mBot.entities) {
+      const val = this.bot.mBot.entities[entity]
+      if (
+        val.position.distanceTo(this.bot.mBot.entity.position) > 10 ||
+        val.name != 'Item' ||
+        this.bot.mcData.blocks[(val.metadata[10] as any).blockId] ==
+          undefined ||
+        !this.bot.mcData.blocks[
+          (val.metadata[10] as any).blockId
+        ].name.endsWith('log')
+      )
+        continue
+
+      this.bot.pickup(val)
     }
-    console.log('done')
+  }
+
+  async execute(): Promise<void> {
+    while (true) {
+      const block = await this.findNextLog()
+      if (!block) break
+
+      // get path
+      const goal = new Goals.GoalNear(
+        block.position.x,
+        block.position.y,
+        block.position.z,
+        4
+      )
+
+      console.log('asdf')
+      const path = await this.bot.getPathTo(goal)
+
+      if (path.status == 'timeout') {
+        this.blacklist.push(block.position)
+        console.log(`path ${path.status}`)
+        continue
+      }
+
+      console.log('asdf1')
+      console.log(this.bot.mBot.pathfinder.isMoving())
+      await this.bot.mBot.pathfinder.goto(goal)
+      console.log(this.bot.mBot.pathfinder.isMoving())
+
+      console.log('asdf2')
+      await this.chop(block)
+
+      console.log('asdf3')
+      await this.pickupLogsAround()
+    }
+    console.log('NO MORE LOGS TO CHOP')
   }
 }
