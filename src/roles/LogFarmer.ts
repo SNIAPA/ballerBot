4043fe64256fbea0301a14ffe77f942fa3643c38
Jpnyman <joel.p.nyman@gmail.com>
2022-10-29 22:20:20 +0200

    merge fix

--- src/roles/LogFarmer.ts
+++ src/roles/LogFarmer.ts
@@ -145,4 +145,9 @@ export default class LogFarmer extends Role {
             
         }
     }
+    
+    override registerListeners = () => {
+    }
+    override removeListeners = () => {
+    }
 }
